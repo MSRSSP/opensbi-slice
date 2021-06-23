2bb9b35841a40f98c97feb525f0e63ee6cf4b9d3
ziqiao <ziqiao@cs.unc.edu>
2021-06-23 21:10:25 +0000

    Move sifive_u.dts to test/dtf/ && add Makefile for dts->dtb

--- test/compile_hello.sh
+++ test/compile_hello.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
 
-CROSS_COMPILE=riscv64-linux-gnu-
+CROSS_COMPILE=riscv64-unknown-linux-gnu-
 ${CROSS_COMPILE}gcc -g -nostdlib -nostdinc -e _start -Wl,--build-id=none -Wl,-Ttext=0x80100000 -DUART_SIFIVE -DUART_SIFIVE_BASE=0x10011000 hello_world.S -o hello_world.elf
 ${CROSS_COMPILE}objcopy -O binary hello_world.elf hello_world.bin
--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,15 @@
 set -x
 set -e
 
-BIOS_PATH=../build/platform/generic/firmware/fw_jump.bin
-KERNEL_PATH=../../linux/arch/riscv/boot/Image
-INITRD_PATH=rootfs_riscv64.img
+BIOS_PATH=${TOP}/opensbi/build/platform/generic/firmware/fw_jump.bin
+KERNEL_PATH=${TOP}/linux/arch/riscv/boot/Image
+RAM_DISK_PATH=$TOP/ramdisk.bin
+DTB_PATH=dtf/sifive_u.dtb
 
 qemu-system-riscv64 -M sifive_u -m 256M -display none \
 	-serial stdio -serial telnet:localhost:4321,server,nowait \
 	-dtb sifive_u.dtb -smp 5 \
 	-bios ${BIOS_PATH} \
 	-kernel ${KERNEL_PATH} \
-	-append "root=/dev/ram rw console=ttySIF0 earlycon=sbi"
-	#-device loader,file=./hello_world.bin,addr=0x80100000
-	#-initrd <initrd_build_path>/rootfs_riscv64.img
+	-drive file=$RAM_DISK_PATH,format=raw,id=hd0 \
+	-device loader,file=./hello_world.bin,addr=0x80100000
