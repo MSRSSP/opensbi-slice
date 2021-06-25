128ab43e55d1e4e11bcd8c29261e3131226b637e
ziqiao <ziqiao@cs.unc.edu>
2021-06-25 22:12:09 +0000

    Fix memory overlap issue in device tree file.

--- test/run.sh
+++ test/run.sh
@@ -6,11 +6,11 @@ KERNEL_PATH=${TOP}/linux/arch/riscv/boot/Image
 RAM_DISK_PATH=$TOP/ramdisk.bin
 DTB_PATH=dtf/sifive_u.dtb
 QEMU=${TOP}/qemu/build/qemu-system-riscv64
-${QEMU} -M sifive_u -m 1G -display none \
+${QEMU} -M sifive_u -m 512M -display none \
 	-serial stdio -serial telnet:localhost:4321,server  \
 	-dtb ${DTB_PATH} -smp 5 \
 	-bios ${BIOS_PATH} \
 	-kernel ${KERNEL_PATH} \
 	-drive file=$RAM_DISK_PATH,format=raw,id=hd0 \
-	-device loader,file=${KERNEL_PATH},addr=0x8d200000 \
+	-device loader,file=${KERNEL_PATH},addr=0x90200000 \
 	 -monitor telnet:127.0.0.1:4322,server,nowait;
