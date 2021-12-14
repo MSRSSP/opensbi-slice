6f67d0aff6e3d43157b8b13abfd559194751e770
Ziqiao Zhou <ziqiao@DESKTOP-896RKGB.localdomain>
2021-12-13 22:01:26 -0800

    Add a dummy sliceloader.c to call both slice_loader_start and slice_loader_finish for figure out required source files.

--- lib/slice/sliceloader.mk
+++ lib/slice/sliceloader.mk
@@ -1,7 +1,3 @@
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/fdt/fdt_domain.c  
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/libfdt/*.c  
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/*/*.S 
-#LIBS-sliceloader += thirdparty/opensbi/lib/slice/slice_fdt.c
 LIBS-sliceloader += thirdparty/opensbi/lib/slice/slice_pmp.c
 LIBS-sliceloader += thirdparty/opensbi/lib/slice/slice.c
 LIBS-sliceloader += thirdparty/opensbi/lib/slice/slice_loader.c
@@ -12,44 +8,36 @@ LIBS-sliceloader += modules/misc/c_stubs.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/riscv_asm.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/riscv_atomic.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_console.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/libfdt/*.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/riscv_hardfp.c
 LIBS-sliceloader += modules/misc/assert.c
 LIBS-sliceloader +=  application/hart0/hss_clock.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_bitmap.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_bitops.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_ipi.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_domain.c
-#LIBS-sliceloader += sbi_ecall.c
-#LIBS-sliceloader += sbi_ecall_base.c
-#LIBS-sliceloader += sbi_ecall_hsm.c
-#LIBS-sliceloader += sbi_ecall_legacy.c
-#LIBS-sliceloader += sbi_ecall_replace.c
-#LIBS-sliceloader += sbi_ecall_vendor.c
-#LIBS-sliceloader += sbi_emulate_csr.c
 LIBS-sliceloader += modules/misc/csr_helper.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_domain.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_hart.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_math.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_hfence.S
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_hsm.c
-#LIBS-sliceloader += sbi_illegal_insn.c
-#LIBS-sliceloader += sbi_init.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_ipi.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_misaligned_ldst.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_platform.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_scratch.c
 LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_string.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/serial/fdt_serial_uart8250.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/serial/uart8250.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/utils/fdt/fdt_helper.c
-#LIBS-sliceloader += thirdparty/opensbi/lib/sbi/sbi_system.c LIBS-sliceloader +=
-#sbi_timer.c LIBS-sliceloader += sbi_tlb.c LIBS-sliceloader +=
-#thirdparty/opensbi/lib/sbi/sbi_unpriv.c LIBS-sliceloader += sbi_expected_trap.c
-#LIBS-sliceloader += modules/debug/hss_debug.c LIBS-sliceloader +=
-#application/hart0/hss_clock.c LIBS-sliceloader += modules/misc/csr_helper.c 
 LIBS-sliceloader += modules/misc/stack_guard.c 
-#LIBS-sliceloader += baremetal/polarfire-soc-bare-metal-library/src/platform/mpfs_hal/common/mss_irq_handler_stubs.c 
+HEADER-sliceloader += thirdparty/opensbi/include/slice/slice_pmp.h
+HEADER-sliceloader += thirdparty/opensbi/include/slice/slice.h
+HEADER-sliceloader += thirdparty/opensbi/include/slice/slice_loader.h
+HEADER-sliceloader += thirdparty/opensbi/include/slice/slice_loader_start.h
+HEADER-sliceloader += thirdparty/opensbi/include/slice/slice_loader_finish.h
+HEADER-sliceloader +=  modules/debug/hss_debug.h
+HEADER-sliceloader += modules/misc/c_stubs.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/riscv_asm.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/riscv_atomic.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_console.h
+HEADER-sliceloader += modules/misc/assert.h
+HEADER-sliceloader +=  application/hart0/hss_clock.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_bitops.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_ipi.h
+HEADER-sliceloader += modules/misc/csr_helper.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_domain.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_hart.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_math.h
+HEADER-sliceloader += thirdparty/opensbi/include/sbi/sbi_string.h
+HEADER-sliceloader += modules/misc/stack_guard.h
 INCLUDE += boards/slice/fpga_design_config/
 INCLUDE += /thirdparty/opensbi/include/ 
 INCLUDE += thirdparty/opensbi/include/sbi 
@@ -66,11 +54,15 @@ INCLUDE += baremetal/polarfire-soc-bare-metal-library/src/platform
 INCLUDE += boards/slice
 INCLUDE += ./
 DEP_FLAGS = $(foreach obj, $(LIBS-sliceloader), /home/ziqiao/riscv/slice-hss/$(obj))
+
 INCLUDE_FLAGS = $(foreach obj,$(INCLUDE), -I/home/ziqiao/riscv/slice-hss/$(obj))
 c:
 	mkdir -p sliceloader/c
 	cmd = $(foreach obj,$(LIBS-sliceloader),cp /home/ziqiao/riscv/slice-hss/$(obj) sliceloader/c/;)
 	echo $(cmd)
+	cmd = $(foreach obj,$(HEADER-sliceloader),cp /home/ziqiao/riscv/slice-hss/$(obj) sliceloader/c/;)
+	echo $(cmd)
+
 sliceloader/sliceloader:
 	mkdir -p $(shell dirname sliceloader)
 	@echo "hi"
