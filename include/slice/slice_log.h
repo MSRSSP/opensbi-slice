1278b754957dfb2f08709556976632a3f2aaabf8
Ziqiao Zhou <ziqiao@DESKTOP-896RKGB.localdomain>
2022-01-11 21:44:00 -0800

    slice_log_printf: fix verbose level

--- include/slice/slice_log.h
+++ include/slice/slice_log.h
@@ -10,16 +10,14 @@ enum slice_log_type {
   SLICE_LOG_ERR,
 };
 
-#define SLICE_DEBUG_LEVEL SLICE_LOG_INFO
+#define SLICE_VERBOSE_LEVEL SLICE_LOG_INFO
 
-void slice_debug_color(unsigned);
-
-#define slice_log_printf(logLevel, ...) \
-  {                                       \
-    if (logLevel > SLICE_DEBUG_LEVEL) {   \
-      sbi_printf("%s(): ", __func__);     \
-      sbi_printf(__VA_ARGS__);            \
-    }                                     \
+#define slice_log_printf(logLevel, ...)    \
+  {                                        \
+    if (logLevel >= SLICE_VERBOSE_LEVEL) { \
+      sbi_printf("%s(): ", __func__);      \
+      sbi_printf(__VA_ARGS__);             \
+    }                                      \
   }
 
 #define slice_printf(...) slice_log_printf(SLICE_LOG_DEBUG, __VA_ARGS__)
